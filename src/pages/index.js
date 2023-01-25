31b378368316dbbb9883ca81fbbea3a3fcaa5389
hojncode <glicm12@gmail.com>
2023-01-25 14:26:49 +0900

    2.3 Server Side Rendering (SSR) - getServerSideProps 코드 내 fetch 사용 -> props로 _app 컴포넌트로 보내서...pageProps 로 하위 컴포넌트에서 사용가능 하게 만듬.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,21 @@ import { useEffect, useState } from "react";
 
 const API_KEY = "";
 
-export default function Home() {
+export default function Home({ results }) {
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      // const response = await fetch(
-      //   `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-      // );
-      // const json = await response.json();
-      //! 위와 아래 promise는 같음. 형식 통일 유무.
-      const { results } = await (await fetch(`/api/movies`)).json();
-      // console.log(data.results); //==> data의 results 를 객체 할당 으로 위 api 함수로 가져옴.
-      setMovies(results);
-      // console.log(data);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     // const response = await fetch(
+  //     //   `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  //     // );
+  //     // const json = await response.json();
+  //     //! 위와 아래 promise는 같음. 형식 통일 유무.
+  //     const { results } = await (await fetch(`/api/movies`)).json();
+  //     // console.log(data.results); //==> data의 results 를 객체 할당 으로 위 api 함수로 가져옴.
+  //     setMovies(results);
+  //     // console.log(data);
+  //   })();
+  // }, []);
 
   return (
     <div className="container">
@@ -27,8 +27,8 @@ export default function Home() {
       </Head> */}
       {/* Head 를 Seo컴포넌트 처럼 개인화 하여 사용할 수 있다. */}
       <Seo title="Home."></Seo>
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -57,4 +57,16 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+//getServerSideProps 이름은 바꾸면 안된다. 이 코드는 오로지 server 쪽에서만 동작한다.(api를 여기에 작성하면 클라이언트에서 절대 볼 수 없다.)
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
